de622d491a8e360ee80a72f98f6b468a03a12a85
Trung <kalosnguyen@gmail.com>
2018-09-23 15:05:20 +0700

    Calculator_App_Final

---
+++ App.js
@@ -0,0 +1,168 @@
+
+'use strict';
+
+import React, { Component } from 'react';
+import {
+    View,
+    Text,
+    AppRegistry,
+    BackAndroid,
+    Alert
+} from 'react-native';
+
+import Style from './src/Style';
+import InputButton from './src/InputButton';
+
+const inputButtons = [
+    [1, 2, 3, '/'],
+    [4, 5, 6, '*'],
+    [7, 8, 9, '-'],
+    [0, '.', '=', '+'],
+    ['DEL', 'AC']
+];
+
+class ReactCalculator extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.initialState = {
+            previousInputValue: 0,
+            inputValue: 0,
+            selectedSymbol: null
+        };
+
+        this.state = this.initialState;
+    }
+
+    componentDidMount() {
+        BackAndroid.addEventListener('hardwareBackPress', this.onBackPress);
+      }
+     
+      onBackPress() {
+        Alert.alert('Confirm Exit', 'Do you want to quit Calculator?', [
+          {
+            text: 'Cancel',
+            onPress: () => console.log('Cancel Pressed!')
+          }, {
+            text: 'OK',
+            onPress: () => BackAndroid.exitApp()
+          }
+        ])
+        return true
+      }
+     
+      componentWillUnmount() {
+        BackAndroid.removeEventListener('hardwareBackPress', this.onBackPress)
+      }
+
+
+    render() {
+        return (
+            <View style={Style.rootContainer}>
+                <View style={Style.displayContainer}>
+                    <Text style={Style.displayText}>{this.state.inputValue}</Text>
+                </View>
+                <View style={Style.inputContainer}>
+                    {this._renderInputButtons()}
+                </View>
+            </View>
+        );
+    }
+
+    _renderInputButtons() {
+
+        let views = inputButtons.map((row, idx) => {
+            let inputRow = row.map((buttonVal, columnIdx) => {
+                return <InputButton
+                            value={buttonVal}
+                            highlight={this.state.selectedSymbol === buttonVal}
+                            onPress={this._onInputButtonPressed.bind(this, buttonVal)}
+                            key={'butt-' + columnIdx} />;
+            });
+
+            return <View style={Style.inputRow} key={'row-' + idx}>{inputRow}</View>;
+        });
+
+        return views;
+    }
+
+    _onInputButtonPressed(input) {
+        switch (typeof input) {
+            case 'number':
+                return this._handleNumberInput(input);
+            default:
+                return this._handleStringInput(input);
+        }
+    }
+
+    _handleNumberInput(num) {
+        let tempvalue=this.state.inputValue;
+        tempvalue=tempvalue.toString();
+        tempvalue=tempvalue.slice(-1);
+        if(tempvalue!== '.'){
+            let inputValue = (this.state.inputValue * 10) + num;
+
+        this.setState({
+            inputValue: inputValue
+        });
+        }
+        else{
+            let inputValue=this.state.inputValue + num;
+            this.setState(
+                {
+                    inputValue: inputValue
+                }
+            );
+        }
+    }
+
+    _handleStringInput(str) {
+        switch (str) {
+            case '/':
+            case '*':
+            case '+':
+            case '-':
+                this.setState({
+                    selectedSymbol: str,
+                    previousInputValue: this.state.inputValue,
+                    inputValue: 0
+                });
+                break;
+
+            case '=':
+                let symbol = this.state.selectedSymbol,
+                    inputValue = this.state.inputValue,
+                    previousInputValue = this.state.previousInputValue;
+
+                if (!symbol) {
+                    return;
+                }
+
+                this.setState({
+                    previousInputValue: 0,
+                    inputValue: eval(previousInputValue + symbol + inputValue),
+                    selectedSymbol: null
+                });
+                break;
+
+            case 'DEL':
+                this.setState(this.initialState);
+                    break;
+
+            case 'AC':
+                this.setState({inputValue: Math.floor(this.state.inputValue/10)});
+                break;
+            
+            case '.':
+            
+            //let lastletter = this.state.inputValue + str;
+            this.setState({inputValue:this.state.inputValue + str});
+           
+           
+        }
+    }
+
+}
+
+export default ReactCalculator;
